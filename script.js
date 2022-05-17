c2cff6ebbf754202f11b3ce1b60d4fafdb33b5ae
LuizHenri16 <lbastos722@gmail.com>
2022-05-17 17:28:29 -0300

    add cards

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let DevicesButton = document.getElementById("Devices")
 let buyLink = document.getElementById("Buy")
 
 let HomeSection = document.getElementById("home")
+let Cards = document.getElementById("cards")
 
 let phoneImage = document.getElementById("phone-image")
 let displaySection = document.getElementById("display")
@@ -10,7 +11,7 @@ let chipsetSection = document.getElementById("chipset")
 let connectionSection = document.getElementById("connection")
 
 let cameraSection = document.getElementById("camera")
-let memoriaSection = document.getElementById("memoria")
+let memorySection = document.getElementById("memory")
 let freaturesSection = document.getElementById("freatures")
 
 
@@ -19,11 +20,23 @@ function Home() {
 
     /* Reset Window */
     displaySection.innerHTML = ""; chipsetSection.innerHTML = "";connectionSection.innerHTML = ""
-    cameraSection.innerHTML = "";memoriaSection.innerHTML = "";freaturesSection.innerHTML = ""; phoneImage.setAttribute("style", "background: none;");
+    cameraSection.innerHTML = "";memorySection.innerHTML = "";freaturesSection.innerHTML = ""; phoneImage.setAttribute("style", "background: none;");
     buyLink.innerHTML = "";
     DeviceList.classList.add ("device-off")
-
+    DeviceList.classList.remove ("open")
+    DevicesButton.setAttribute("style", "default")
+    
     /*Home Section */
+
+    HomeSection.innerHTML = `<div id='cards' class='cards'>
+    <div class='section1'>
+        <div id='card1' class='card' onclick="m52Specs()">Galaxy M52 5G</div> <div id='card2' class='card' onclick="iphone12Specs()">Iphone 12 Pro Max</div>   <div id='card3' onclick="iphone13Specs()" class='card'>Iphone 13 Pro</div>   <div id='card4' onclick="m4proSpecs()" class='card'>Xiaomi Poco M4 Pro 5G</div> 
+    </div>
+
+    <div class='section2'>
+        <div id='card5' class='card'onclick="s22UltraSpecs()" >Galaxy S22 Ultra</div>   <div id='card6' onclick="x4proSpecs()" class='card'>Xiaomi Poco X4 Pro 5G</div>   <div id='card7' onclick="a52Specs()" class='card'>Galaxy A52</div>
+    </div>
+</div>`
 }
 
 window.addEventListener("load", () => {
@@ -93,7 +106,7 @@ function m52Specs() {
                                    <p>HDR</p>
                                    <p>Estabilizador</p>
                                    `
-    memoriaSection.innerHTML = `<h2>Armazenamento</h2>
+    memorySection.innerHTML = `<h2>Armazenamento</h2>
                                 <p>128GB ROM</p>
                                 <p>max 1028GB</p>
                                 <p>6GB+4 RAM</p>
@@ -144,7 +157,7 @@ function iphone12Specs() {
                                     <p>HDR</p>
                                     <p>Estabilização Ótica/</p>
                                     `
-    memoriaSection.innerHTML = `<h2>Armazenamento</h2>
+    memorySection.innerHTML = `<h2>Armazenamento</h2>
                                  <p>512GB ROM</p>
                                  <p>6GB</p>
                                  <p>Sem expansão</p>
@@ -195,7 +208,7 @@ function iphone13Specs() {
                                     <p>HDR</p>
                                     <p>Estabilização Ótica/</p>
                                     `
-    memoriaSection.innerHTML = `<h2>Armazenamento</h2>
+    memorySection.innerHTML = `<h2>Armazenamento</h2>
                                  <p>Até 1000GB ROM</p>
                                  <p>6GB</p>
                                  <p>Sem expansão</p>
@@ -245,7 +258,7 @@ function m4proSpecs() {
                                     <p>HDR</p>
                                     <p>Estabilização Ótica/</p>
                                     `
-    memoriaSection.innerHTML = `<h2>Armazenamento</h2>
+    memorySection.innerHTML = `<h2>Armazenamento</h2>
                                  <p>Até 128GB ROM</p>
                                  <p>4GB RAM</p>
                                  <p>MicroSD até 1024GB</p>
@@ -294,7 +307,7 @@ function s22UltraSpecs() {
                                     <p>8K/24fps + 4k/60fps</p>
                                     <p>HDR</p>
                                     `
-    memoriaSection.innerHTML = `<h2>Armazenamento</h2>
+    memorySection.innerHTML = `<h2>Armazenamento</h2>
                                  <p>Max 512GB ROM</p>
                                  <p>12GB RAM</p>
                                  <p>Não expande</p>
@@ -342,7 +355,7 @@ function a52Specs() {
                                     <p>4K(2160p)/30fps</p>
                                     <p>HDR</p>
                                     `
-    memoriaSection.innerHTML = `<h2>Armazenamento</h2>
+    memorySection.innerHTML = `<h2>Armazenamento</h2>
                                  <p>128GB ROM</p>
                                  <p>6GB RAM</p>
                                  <p>Max 1024Gb</p>
@@ -354,3 +367,51 @@ function a52Specs() {
     DeviceList.classList.remove("open")                              
 }
 
+function x4proSpecs() {
+    deviceClassRemove()
+    HomeSection.innerHTML = ""
+    buyLink.innerHTML = `<a href="https://www.amazon.com.br/Smartphone-Xiaomi-X4-Pro-5G/dp/B09TD7B23K/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=TD7FGFD896OV&keywords=poco+x4+pro&qid=1652814925&s=electronics&sprefix=poco+x4+por%2Celectronics%2C242&sr=1-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147" target="v_blank">Buy</a>`
+    phoneImage.setAttribute("style", `
+                                width: 26rem;
+                                height:30rem;
+                                background: url('assets/x4pro/x4pro.jpg');
+                                background-position-x: -.5rem;
+                                background-size: contain;
+                                background-repeat: no-repeat;
+                                background-size:cover;
+                                background-clip: content-box;
+                                margin-left: -2rem;
+                                margin-top: 3rem;
+                                `)
+    displaySection.innerHTML = `<h2>Display</h2>
+                                 <p>6.67 Polegadas</p>
+                                 <p>1080x2400px</p>
+                                 <p>AMOLED</p>
+                                 <p>120hz</p>
+                                `
+    chipsetSection.innerHTML = `<h2>Chipset</h2>
+                                 <p>Snapdragon 695</p>
+                                 <p>Adreno 619</p>
+                                 <p>2x 2.2Ghz + 6x 1.7Ghz</p>
+                                 `
+    connectionSection.innerHTML = `<h2>Conexão</h2>
+                                 <p>WIFI 802.11 a/b/g/n/ac/</p>
+                                 <p>Bluetooth 5.1 com A2DP/LE</p>
+                                 <p>2500mbps / 1500mbps</p>
+                                 `
+    cameraSection.innerHTML = `<h2>Camera</h2>
+                                    <p>108Mp + 8Mp + 2Mp / 16Mp</p>
+                                    <p>1080p/30fps</p>
+                                    <p>HDR</p>
+                                    `
+    memorySection.innerHTML = `<h2>Armazenamento</h2>
+                                 <p>128GB ROM</p>
+                                 <p>6GB RAM</p>
+                                 <p>Max 1024Gb</p>
+                                 `
+    freaturesSection.innerHTML = `<h2>Freatures</h2>
+                                   <p>Face Detection</p>
+                                   <p>Bateria 5000maH</p>
+                                   `
+    DeviceList.classList.remove("open")                              
+}
